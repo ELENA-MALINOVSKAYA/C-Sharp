3c327abd843e8773df9ea6b8626dc3f1c00743f4
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-03 00:40:42 +0300

    Add HomeWork1

---
+++ Seminar1/HomeWork1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter 2 number");
+int num1=Convert.ToInt32(Console.ReadLine());
+int num2=Convert.ToInt32(Console.ReadLine());
+if (num2>num1){
+    Console.WriteLine($"min = {num1}, max = {num2}");
+}
+else {
+    Console.WriteLine($"min = {num2}, max = {num1}");
+}   
