bdacdd12aa9f55c126560eaa0c6b42282c480dc0
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-03 02:26:23 +0300

    Add home work 3

---
+++ Seminar1/HomeWork3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter number");
+int num=Convert.ToInt32(Console.ReadLine());
+if (num%2==0){
+    Console.WriteLine("Число является четным");
+}
+else {
+    Console.WriteLine("Число не является четным");
+}
