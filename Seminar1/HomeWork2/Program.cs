f22e6a9dc5eba931a2cc51ab712f2f357624b0a4
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-03 02:11:51 +0300

    Add home work 2

---
+++ Seminar1/HomeWork2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter 3 numbers");
+int num1=Convert.ToInt32(Console.ReadLine());
+int num2=Convert.ToInt32(Console.ReadLine());
+int num3=Convert.ToInt32(Console.ReadLine());
+int max=num1;
+if (num2>max) max=num2;
+if (num3>max) max=num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
