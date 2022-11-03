cfac55e4831c717d4e01125614ade1179996f403
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-03 09:23:18 +0300

    Created a sequence of numbers

---
+++ Seminar1/HomeWork4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter number");
+int num=Convert.ToInt32(Console.ReadLine());
+int numStart=1;
+if (num<1){
+    Console.WriteLine("Число меньше 1.");
+}
+
+else {while(numStart!=num){
+    Console.Write(numStart + ", ");
+    numStart++;
+
+} 
+    Console.Write(num);
+}
