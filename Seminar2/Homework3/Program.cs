4bb178c11209a45275f3ec00bbc31704f55f1b68
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-05 02:29:50 +0300

    added a solution to the homework_3 Seminar_2

---
+++ Seminar2/Homework3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1: 
+        Console.WriteLine("рабочий день");
+        break;
+    case 2: 
+        Console.WriteLine("рабочий день");
+        break;
+    case 3: 
+        Console.WriteLine("рабочий день");
+        break;
+    case 4: 
+        Console.WriteLine("рабочий день");
+        break;
+    case 5: 
+        Console.WriteLine("рабочий день");
+        break;
+    case 6: 
+        Console.WriteLine("Выходной");
+        break;
+    case 7: 
+        Console.WriteLine("Выходной");
+        break;
+    default:
+        Console.WriteLine("Вне диапазона");
+        break;
+}
