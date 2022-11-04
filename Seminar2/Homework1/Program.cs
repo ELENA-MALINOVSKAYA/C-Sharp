c958834546b48bda4360e88f72795c5ab1c58f66
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-05 00:04:34 +0300

    add homework_1 Seminar_2

--- Seminar2/Homework1/Program.cs
+++ Seminar2/Homework1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число");
+int num=Convert.ToInt32(Console.ReadLine());
+//int num1= num /100;
+int num2= (num/10) % 10;
+//int num3= num % 10;
+
+
+if (num<100 || num>999){
+    Console.WriteLine("Введенное число не трехзначное");
+}
+else {
+    Console.WriteLine($"Вторая цифра числа -> {num2}");
+}
