c2bd6cc3efafb739f293c20996fa1c55e29a244b
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-05 01:53:30 +0300

    added a solution to the homework_2 Seminar_2

---
+++ Seminar2/Homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = num.ToString();
+int [] array = new int[str.Length];
+int n = array.Length;
+
+for( int i=0; i< str.Length; i++)
+{
+     array[i] = int.Parse(str[i].ToString());
+}
+
+if (n<3){
+    Console.WriteLine("третьей цифры нет");
+}
+else {
+    Console.WriteLine($"Третья цифра числа {num}-> {array[2]}");
+}
