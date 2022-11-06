2143c049c6fc9edeec9f27091f5d136b1ea119fd
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-07 00:17:23 +0300

    solving the task

---
+++ Seminar2/Homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num<100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else 
+{
+    for( int i=0; num> 999; i++)
+{
+     num = num /10;
+}
+Console.WriteLine($"Третья цифра числа -> {num % 10}");
+}
