3ff03235f5a54a1f48c63882c40549ae8e953c57
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-07 09:30:27 +0300

    add metod in  HW3

--- Seminar3/Homework3/Program.cs
+++ Seminar3/Homework3/Program.cs
@@ -1,12 +1,18 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
-Console.WriteLine("Введите число:");
-double N = double.Parse(Console.ReadLine());
 
-for (double i = 1; i < N; i++)
-{
-    Console.Write($"{Math.Pow(i,3)}, ");
+static double TableOfCubes1ToN(double N)
+{   
     
+    double res = 0;
+    for (int i = 1; i <= N; i++) 
+    {
+    res = Math.Pow(i,3);
+    Console.Write($"{res} ");
+    }
+    return res;
+  
 }
-Console.Write(Math.Pow(N,3));
+Console.WriteLine("Введите число:");
+double res = TableOfCubes1ToN(double.Parse(Console.ReadLine()));
