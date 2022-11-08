a769edc8b0b7e911115c60b6e50cc84f06737365
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-09 00:40:20 +0300

    interim solution

---
+++ Seminar4/Homework3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Console.WriteLine("Введите размер массива");
+
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("заполните массив");
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    int element = int.Parse(Console.ReadLine());
+    array[i] = element;
+    Console.Write(array[i]);
+}
+
