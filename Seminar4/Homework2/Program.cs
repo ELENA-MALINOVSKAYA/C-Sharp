25e5764bf002a62b316f1b586cae7c8937ee87e6
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-08 13:27:43 +0300

    save folder HW2

---
+++ Seminar4/Homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 0; num > 9; i++)
+{
+    sum = sum+ num%10;
+}
+// for (int i = 0; num > 9; i++)
+//{
+    num = num/10;
+//}
+//sum = sum + num;
+Console.WriteLine($"Сумма чисел цифры {num} = {sum}");
