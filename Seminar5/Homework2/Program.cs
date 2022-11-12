e1bde4fad428b460d5e0f1c08cb6b4b853851be1
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-13 01:40:33 +0300

    task solution HW2

---
+++ Seminar5/Homework2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int[] NewArr = GetRandomArray(8);
+PrintArray(NewArr);
+int sum = SumOddInteger(NewArr);
+System.Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях = {sum}");
+
+
+int[] GetRandomArray(int size, int begin = 0, int last = 100)
+{
+int[] arr = new int[size];
+Random random = new Random();
+
+for (int i = 0; i < size; i++)
+{
+arr[i] = random.Next(begin, last + 1);
+}
+
+return arr;
+}
+
+void PrintArray(int[] arr)
+{
+System.Console.WriteLine($"[{String.Join(",",arr)}]");
+}
+
+
+int SumOddInteger(int[] arr)
+{
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{   
+    if(i % 2 != 0)
+    {
+    sum+=arr[i];
+    }
+}
+return sum;
+}
