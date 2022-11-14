bdfe77423819be2ea02d9debe5de59c844402f2d
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-14 15:34:52 +0300

    task solution

---
+++ Seminar6/Homework1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Введите число");
+
+int[] array = ArrayConvertStringToInt();
+int count = CountPositiveNumbers(array);
+Console.WriteLine($"Количество чисел больше 0 ={count}");
+
+
+int[] ArrayConvertStringToInt()
+{
+string str = Console.ReadLine();
+string[] strArr = str.Split(" ");
+int[] array = new int[strArr.Length];
+
+for( int i=0; i< strArr.Length; i++)
+{
+    array[i] = int.Parse(strArr[i]);
+}
+return array;
+}
+
+
+int CountPositiveNumbers(int[] arr)
+{
+int count=0;
+for (int i = 0; i < arr.Length; i++)
+{   if (arr[i] > 0)
+{
+    count++;
+}
+}
+return count;
+}
