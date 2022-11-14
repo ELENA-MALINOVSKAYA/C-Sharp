5b0e9899d9d1365671205beba1dd53efa27d73a4
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-14 16:01:27 +0300

    save metod

--- Metod/Program.cs
+++ Metod/Program.cs
@@ -21,3 +21,29 @@ System.Console.Write(arr[i] + " ");
 // System.Console.Write($"{arr[i]} ");
 }
 }
+
+void PrintArray1(int[] arr)
+{
+System.Console.Write($"[{String.Join(", ",arr)}]");
+}
+
+Clear();
+Write("Введите стороны треугольника через пробел: ");
+
+string[] st=ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+if(IsTriangle(int.Parse(st[0]),int.Parse(st[1]),int.Parse(st[2])))
+{
+WriteLine("Да");
+}
+else
+{
+WriteLine("Нет");
+}
+
+
+
+bool IsTriangle(int a,int b,int c)
+{
+return (((a+b)>c)&&((a+c)>b)&&((b+c)>a));
+}
