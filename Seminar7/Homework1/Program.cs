df23a072544d2cb6e93f8eefc3cf256901ba633c
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-17 19:48:16 +0300

    delete string

--- Seminar7/Homework1/Program.cs
+++ Seminar7/Homework1/Program.cs
@@ -4,46 +4,31 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-// Задача 50. Напишите программу, которая на вход принимает элемент в двумерном массиве, и возвращает индекс этого элемента или 
-//же указание, что такого элемента нет.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 17 -> такого числа в массиве нет
-
-// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
-    
-    
-    int row = Setnumbers("m");
-    int column = Setnumbers("n");
-    double[,] matrix = GetRandomMatrix(row,column);
-    PrintMatrix(matrix);
-
-    int Setnumbers(string name)
-    {   
-        string[] arr = name.Split(" ");
-        Console.WriteLine($"Enter numbers {name}");
-        int num = int.Parse(Console.ReadLine());
-        return num;
-    }
 
 
-double[,] GetRandomMatrix(int rows, int columns, int begin=-10, int last=10)
-{   
-    double[,] matrix = new double[rows,columns];
+int row = Setnumbers("m");
+int column = Setnumbers("n");
+double[,] matrix = GetRandomMatrix(row, column);
+PrintMatrix(matrix);
+
+int Setnumbers(string name)
+{
+    string[] arr = name.Split(" ");
+    Console.WriteLine($"Enter numbers {name}");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+
+double[,] GetRandomMatrix(int rows, int columns, int begin = -10, int last = 10)
+{
+    double[,] matrix = new double[rows, columns];
     var random = new Random();
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
         {
-            matrix[i,j] = random.Next(begin,last+1) + random.NextDouble();
+            matrix[i, j] = random.Next(begin, last + 1) + random.NextDouble();
         }
     }
     return matrix;
@@ -51,13 +36,13 @@ double[,] GetRandomMatrix(int rows, int columns, int begin=-10, int last=10)
 
 void PrintMatrix(double[,] matrix)
 {
-for (int i = 0; i < matrix.GetLength(0); i++)
-{ 
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        System.Console.Write(matrix[i,j] + " ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
-}
 }
 
