241ab1092a48b6071718848180f962a9a0246e46
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-22 15:19:19 +0300

    add task 56

--- Test/Program.cs
+++ Test/Program.cs
@@ -71,8 +71,9 @@ void Start()
                     PrintMatrix(matrix5);
                     System.Console.WriteLine();
                     PrintMatrix(FindSummEachRow(matrix5));
-                    int minPosition = MinPositionInColumn(matrix5);
-                    System.Console.WriteLine($"{minPosition} строка");
+                
+                    int minPosition = MinPositionInColumn(FindSummEachRow(matrix5))+1;
+                    System.Console.WriteLine($"Минимальная сумма элементов находится в {minPosition} строке");
                 }
                 else { System.Console.WriteLine("Двумерный массив не прямоугольный"); }
 
@@ -305,15 +306,15 @@ int[,] FindSummEachRow(int[,] matrix)
 
 int MinPositionInColumn(int[,] matrix)
 {
-    int minPosition = 1;
+    int minPosition = 0;
+    int min = matrix[0, 1];
 
-    for (int i = 1; i < matrix.GetLength(1); i++)
+
+    for (int i = 1; i < matrix.GetLength(0); i++)
     {
-        int min = matrix[2, i];
-
-        if (matrix[2, i] < min) continue;
+        if (matrix[i, 1] < min) 
         {
-            min = matrix[2, i];
+            min = matrix[i, 1];
             minPosition = i;
         }
 
